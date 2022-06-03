dfd467a796903fa4ae10b95fc48c3f988ef71a4b
Vladimir Protcay <protcay2@mail.ru>
2022-06-03 23:25:12 +0300

    dz4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,106 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  // Ваш код
+  this.name  = name;
+  this.gender = gender;
+  this.age = age;
 }
+const StudentI = new Student("Eduard", "men", 17);
+const StudentM = new Student("Latifa", "women", 18);
+const StudentN = new Student("Sigizmund", "men", 19);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+ if(this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);  
+    }
+   else {
+     this.marks.push(mark);  
+ }
+}
+
+Student.prototype.addMarks = function (...mark) { 
+  if(this.marks === undefined) {
+    this.marks = [];
+    this.marks.push.apply(this.marks, mark);  
+    }
+  else {
+    this.marks.push.apply(this.marks, mark);  
+  }
+}
+
+Student.prototype.getAverage = function (mark) {
+    let average = this.marks.reduce((acc, item, idx, arr) => {
+    acc += item;
+    if (idx === arr.length - 1) {
+      return this.average  = acc / arr.length;
+    } 
+    else {
+      return acc;
+    }
+  })
+  return average
+}
+
+
+
+Student.prototype.excluded = function (reason) {
+  delete this.subject  
+  delete this.marks 
+  delete this.average   
+  this.excluded = reason;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
